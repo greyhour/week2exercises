462b07e9c69a52debde9fb854bb9a5297c2a28f0
Lukas Roessner <30021032@student.toiohomai.ac.nz>
2019-03-06 16:38:30 +1300

    exercise1 exercise2

---
+++ Exercise1/Exercise1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<CreditCard> CreditCards = new List<CreditCard>();
+            int counter = 0;
+            Boolean exit = false;
+            do
+            {
+                Console.WriteLine("Would you like to create a CreditCard? (y/n):");
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "y":
+                        Console.Clear();
+
+                        Console.WriteLine("Please enter the Name: ");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("Please enter the Credit Card Number: ");
+                        long creditcard = Convert.ToInt64(Console.ReadLine());
+                        Console.WriteLine("Please enter the CSC: ");
+                        int csc = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Please enter the expiry Date: ");
+                        string date = Console.ReadLine();
+
+                        CreditCards.Add(new CreditCard(name, creditcard, csc, date));
+                        counter++;
+                        break;
+                    case "n":
+                        exit = true;
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.WriteLine(input + " is not a recognized input!");
+                        break;
+                }
+            } while (counter < 5 && exit == false);
+
+            Console.Clear();
+            foreach (CreditCard x in CreditCards)
+            {
+                Console.Write("Name: " + x.Name + ", ");
+                Console.Write(", Date: " + x.Date);
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+
+    class CreditCard
+    {
+        internal string Name { get; set; }
+        internal long CreditNum { private get; set; }
+        internal int CSC { private get; set; }
+        internal string Date { get; set; }
+
+        internal string DisplayPrivateData()
+        {
+            return "Credit Card Number: " + CreditNum + ", CSC: " + CSC;
+        }
+
+        public CreditCard(string name, long creditcard, int csc, string date)
+        {
+            Name = name;
+            CreditNum = creditcard;
+            CSC = csc;
+            Date = date;
+        }
+    }
+}
---
+++ Exercise2/Exercise2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Employee
+    {
+        public string Name { get; set; }
+        public long Phone { get; set; }
+        public string Email { get; set; }
+        public int IRD { get; set; }
+        public int BankAccount { get; set; }
+    }
+}
