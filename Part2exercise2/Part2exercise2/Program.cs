a767b86f03c8a0b23cce9c50f4c5e0215d822b92
greyhour <30021032@student.toiohomai.ac.nz>
2019-03-07 10:45:44 +1300

    part2exe2continued

--- Part2exercise2/Part2exercise2/Program.cs
+++ Part2exercise2/Part2exercise2/Program.cs
@@ -12,4 +12,31 @@ namespace Part2exercise2
         {
         }
     }
+
+    class Customer
+    {
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string PaymentMethod { get; set; }
+        public ShoppingCart Cart { get; set; }
+
+
+        public Customer(string name, string username, string password, string paymentmethod, ShoppingCart cart)
+        {
+            Name = name;
+            Username = username;
+            Password = password;
+            PaymentMethod = paymentmethod;
+            Cart = cart;
+        }
+    }
+
+    class ShoppingCart
+    {
+        public string ItemName { get; set; }
+        public int ProductCode { get; set; }
+        public int ItemCost { get; set; }
+        public int TotalCosts { get; set; }
+    }
 }
