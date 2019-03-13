c3f791f0a1b301aeb86e58d7c581d4f508ed732b
greyhour <30021032@student.toiohomai.ac.nz>
2019-03-13 16:20:48 +1300

    e2p3

--- Part2exercise2/Part2exercise2/Program.cs
+++ Part2exercise2/Part2exercise2/Program.cs
@@ -10,33 +10,71 @@ namespace Part2exercise2
     {
         static void Main(string[] args)
         {
+            Person p1 = new Person("Sam", "sm487", "Password1", "Credit Card");
+            
+            p1.AddToCart(123, "Shoes");
+            p1.AddToCart(5423, "Hat");
+
+            List<Array> items = new List<Array>();
+            foreach(string x in p1.GetItems())
+            {
+                string[] words = x.Split(':');
+                items.Add(words);
+                Console.WriteLine("-----ID: " + words[0] + "-----");
+                Console.WriteLine("Name: " + words[1]);
+            }
+            Console.ReadLine();
         }
     }
 
-    class Customer
+    public class Person
     {
+        public static int counter = 0;
         public string Name { get; set; }
-        public string Username { get; set; }
+        public string UserName { get; set; }
         public string Password { get; set; }
         public string PaymentMethod { get; set; }
-        public ShoppingCart Cart { get; set; }
+        public List<Item> ShoppingCart { get; set; }
+        
 
 
-        public Customer(string name, string username, string password, string paymentmethod, ShoppingCart cart)
+        public Person(string name, string username, string password, string paymentmethod)
         {
             Name = name;
-            Username = username;
+            UserName = username;
             Password = password;
             PaymentMethod = paymentmethod;
-            Cart = cart;
+            counter++;
+        }
+
+        public void AddToCart(int id, string name)
+        {
+            if (ShoppingCart == null)
+            {
+                ShoppingCart = new List<Item>();
+            }
+            ShoppingCart.Add(new Item(id, name));
+        }
+        public List<string> GetItems()
+        {
+            List<string> items = new List<string>();
+            foreach (Item x in ShoppingCart)
+            {
+                items.Add(x.ID + ":" + x.Name);
+            }
+            return items;
         }
     }
 
-    class ShoppingCart
+    public class Item
     {
-        public string ItemName { get; set; }
-        public int ProductCode { get; set; }
-        public int ItemCost { get; set; }
-        public int TotalCosts { get; set; }
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public Item(int id, string name)
+        {
+            ID = id;
+            Name = name;
+        }
     }
 }
